048d35f9ed328ccf925498f39c2b9dd7b904ed60
juansu01 <thejuank0@hotmail.com>
2021-10-01 17:10:41 -0500

    Adding command to bot

--- holberton_bot.py
+++ holberton_bot.py
@@ -1,5 +1,6 @@
 import discord
 import re
+from intranet_hbn_scraper import intranet_hbn_scraper
 
 client = discord.Client()
 
@@ -15,5 +16,9 @@ async def on_message(message):
     
     if re.match(re.compile("repeat", re.I), message.content):
         await message.channel.send("You just said: {}".format(message.content.replace('repeat', '')))
+
+    if re.match(re.compile("scrap me", re.I), message.content):
+        message_split = message.content.replace("scrap me ", "").split("/")
+        intranet_hbn_scraper(message_split[0], message_split[1])
 
 client.run("TOKEN")
