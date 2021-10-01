d6741c256b8a0f5097dff573e0e8e48fd3cf4373
Felipe Vargas <77789736+felipevargas-bz@users.noreply.github.com>
2021-10-01 23:46:31 +0000

    modify ./api/api_script - transform in a funtion

--- api/api_script.py
+++ api/api_script.py
@@ -4,19 +4,21 @@ import requests
 from getpass import getpass
 from api import get_token
 
-url = 'https://intranet.hbtn.io/users/auth_token.json'
-api_key = get_token.intranet_hbn_scraper("2809@holbertonschool.com", "Julioc8172315")
-user_email = input("Your Holberton email")
-password = getpass("Your intranet password")
-url_token = "users/auth_token.json"
+def get_info(user_email=None, password=None):
+    """
+    .....
+    """
+    url = 'https://intranet.hbtn.io/'
+    api_key = get_token.intranet_hbn_scraper(user_email, password)
+    url_token = "users/auth_token.json"
 
-parameters = {'api_key': api_key,
-              'email': user_email,
-              'password': password,
-              'scope': 'checker',}
+    parameters = {'api_key': api_key,
+                  'email': user_email,
+                  'password': password,
+                  'scope': 'checker',}
 
-try:
-    auth_res = requests.post(url + url_token, data=parameters)
-    auth_token = auth_res['auth_token']
-except:
-    pass
+    try:
+        auth_res = requests.post(url + url_token, data=parameters)
+        auth_token = auth_res['auth_token']
+    except:
+        pass
