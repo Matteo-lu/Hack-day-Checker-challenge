7a482b6e45437e3d3851f5f7912374a438e74ddb
juansu01 <thejuank0@hotmail.com>
2021-10-01 19:01:46 -0500

    Modified command to fit explanation on the repo

--- holberton_bot.py
+++ holberton_bot.py
@@ -17,8 +17,8 @@ async def on_message(message):
     if re.match(re.compile("repeat", re.I), message.content):
         await message.channel.send("You just said: {}".format(message.content.replace('repeat', '')))
 
-    if re.match(re.compile("scrap me", re.I), message.content):
-        message_split = message.content.replace("scrap me ", "").split("/")
+    if re.match(re.compile("Scrap me", re.I), message.content):
+        message_split = message.content.replace("Scrap me ", "").split("/")
         intranet_hbn_scraper(message_split[0], message_split[1])
 
-client.run("TOKEN")
+client.run("ODkzNTg3NDQ0OTEyNDkyNTY1.YVdoKA.5T8r2sKrjMtyY5HvwgP5trEYUq4")
